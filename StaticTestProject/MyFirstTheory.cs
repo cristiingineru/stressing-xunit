67170626dfeb060830a43f11c6441bc339e0296b
cristiingineru <cristi.ingineru@gmail.com>
2016-03-16 22:36:04 +0200

    Initial commit

---
+++ StaticTestProject/MyFirstTheory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StaticTestProject
+{
+    public class MyFirstTheory
+    {
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void IsOddTest(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
---
+++ StaticTestProject/MyFirstTheoryWithPropertyData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace StaticTestProject
+{
+    public class MyFirstTheoryWithPropertyData
+    {
+        [Theory, MemberData("SplitCountData")]
+        public void SplitCount(string input, int expectedCount)
+        {
+            var actualCount = input.Split(' ').Count();
+            Assert.Equal(expectedCount, actualCount);
+        }
+
+        public static IEnumerable<object[]> SplitCountData
+        {
+            get
+            {
+                return Enumerable
+                    .Range(0, 10000)
+                    .Select(i => new object[] { i.ToString(), 1 });
+            }
+        }
+    }
+}
