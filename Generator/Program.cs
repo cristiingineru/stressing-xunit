88c79049f542e4db1eabea8864e32d8e9a86f88b
cristiingineru <cristi.ingineru@gmail.com>
2016-03-29 22:29:43 +0300

    Example update

--- Generator/Program.cs
+++ Generator/Program.cs
@@ -11,7 +11,7 @@ namespace Generator
     {
         static void Main(string[] args)
         {
-            var factCount = 300;
+            var factCount = 3;
             var file = @"..\..\..\WithGeneratedContent\Generated.cs";
 
             var fileContent = Generate(factCount);
